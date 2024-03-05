3240095f2beb50f4d068f5171ccad2f4dfac6b46
teethw <kazisadman01@gmail.com>
2024-03-05 10:12:56 +0600

    creatd class for api response and api error

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,15 @@
+const asyncHandler = (fn) => {
+  return (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
