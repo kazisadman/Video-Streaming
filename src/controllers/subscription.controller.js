5b0ea0cf2d9b5928a73b14eb349a0a7e2c16bf2f
teethw <kazisadman01@gmail.com>
2024-03-14 08:52:05 +0600

    add end point to get subsciber and subscribed channel info

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,8 +1,9 @@
-import mongoose from "mongoose";
+import mongoose, { isValidObjectId } from "mongoose";
 import { apiError } from "../utils/apiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { Subscription } from "../models/subscription.models.js";
 import { apiResponse } from "../utils/apiResponse";
+import { User } from "../models/user.models.js";
 
 const toogleSubscription = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
@@ -30,4 +31,86 @@ const toogleSubscription = asyncHandler(async (req, res) => {
   res.status(200).json(200, {}, "Channel subscribed");
 });
 
-export { toogleSubscription };
+const getChannelSubscribers = asyncHandler(async (req, res) => {
+  const { channelId } = req.params;
+
+  if (!mongoose.isValidObjectId(channelId)) {
+    throw new apiError(404, "Channel not found");
+  }
+
+  const subscribers = await Subscription.aggregate([
+    {
+      $match: {
+        channel: new mongoose.Types.ObjectId(channelId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "channel",
+        foreignField: "_id",
+        as: "subscribers",
+      },
+    },
+    {
+      $unwind: "$subscribers",
+    },
+    {
+      $project: {
+        _id: 0,
+        subscriber: {
+          userName: 1,
+          avatar: 1,
+          _id: 1,
+          fullName: 1,
+        },
+      },
+    },
+  ]);
+
+  res
+    .status(200)
+    .apiResponse(200, subscribers, "Subscribers fetched successfully");
+});
+
+const getSubscribedChannels = asyncHandler(async (req, res) => {
+  const { channelId } = req.params;
+
+  if (!isValidObjectId(channelId)) {
+    throw new apiError(404, "Channel not found");
+  }
+
+  const subscribedTo = await Subscription.aggregate([
+    {
+      $match: {
+        subscriber: new mongoose.Types.ObjectId(channelId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "channel",
+        foreignField: "_id",
+        as: "subscribedTo",
+      },
+    },
+    {
+      $unwind: "$subscribedTo",
+    },
+    {
+      $project: {
+        _id: 0,
+        subscribedTo: {
+          fullName: 1,
+          userName: 1,
+          avatar: 1,
+          _id: 1,
+        },
+      },
+    },
+  ]);
+
+  res.status(200).json(200, subscribedTo, "Subscribed channel info fetched");
+});
+
+export { toogleSubscription, getChannelSubscribers, getSubscribedChannels };
